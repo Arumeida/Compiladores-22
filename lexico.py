3b31c9d7a4915c14048927b3a78f829e37584972
arumeng <mtsarumeida@gmail.com>
2022-09-06 11:33:24 -0300

    finalized rules and added file reader

--- lexico.py
+++ lexico.py
@@ -2,30 +2,29 @@ import sys, os
 import ply.lex as lex
 
 
-class LexAnalyser(object):
-    reserved = {
-        'class' : 'CLASS',
-        'else' : 'ELSE',
-        'false' : 'FALSE',
-        'fi' : 'FI',
-        'if' : 'IF',
-        'in' : 'IN',
-        'inherits' : 'INHERITS',
-        'isvoid' : 'ISVOID',
-        'let' : 'LET',
-        'loop' : 'LOOP',
-        'pool' : 'POOL',
-        'then' : 'THEN',
-        'while' : 'WHILE',
-        'case' : 'CASE',
-        'esac' : 'ESAC',
-        'new' : 'NEW',
-        'of' : 'OF',
-        'not' : 'NOT',
-        'true' : 'TRUE',
-    }
+reserved = {
+    'class' : 'CLASS',
+    'else' : 'ELSE',
+    'false' : 'FALSE',
+    'fi' : 'FI',
+    'if' : 'IF',
+    'in' : 'IN',
+    'inherits' : 'INHERITS',
+    'isvoid' : 'ISVOID',
+    'let' : 'LET',
+    'loop' : 'LOOP',
+    'pool' : 'POOL',
+    'then' : 'THEN',
+    'while' : 'WHILE',
+    'case' : 'CASE',
+    'esac' : 'ESAC',
+    'new' : 'NEW',
+    'of' : 'OF',
+    'not' : 'NOT',
+    'true' : 'TRUE',
+}
 
-    tokens = [
+tokens = [
     'ID',
     'NUM',
     'STR',
@@ -44,43 +43,71 @@ class LexAnalyser(object):
     'OPENPTH',
     'CLOSEPTH',
     'OPENKEYS',
-    'CLOSKEYS',
-    ]
+    'CLOSEKEYS',
+] + list(reserved.values())
 
-    t_PLUS = r'\+'
-    t_MINUS = r'\-'
-    t_MPLY = r'\*'
-    t_DIV = r'\/'
-    t_LESSTH = r'\<'
-    t_LESSEQ = r'\<\='
-    t_EQUALS = r'\='
-    t_ATTR = r'\<\-'
-    t_DOT = r'\.'
-    t_COMMA =r'\,'
-    t_DOUBLEDOT = r'\:'
-    t_COMMADOT = r'\;'
-    t_OPENPTH = r'\('
-    t_CLOSEPTH = r'\)'
-    t_OPENKEYS = r'\{'
-    t_CLOSEKEYS = r'\}'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MPLY = r'\*'
+t_DIV = r'\/'
+t_LESSTH = r'\<'
+t_LESSEQ = r'\<\='
+t_EQUALS = r'\='
+t_ATTR = r'\<\-'
+t_DOT = r'\.'
+# t_COMMA =r'\,'
+t_DOUBLEDOT = r'\:'
+t_COMMADOT = r'\;'
+t_OPENPTH = r'\('
+t_CLOSEPTH = r'\)'
+t_OPENKEYS = r'\{'
+t_CLOSEKEYS = r'\}'
+t_ignore = ' \t'
 
-    def t_ID (self, token):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        token.type = self.reserved.get(t.value.lower(),'ID')
-        return token
+def t_ID (token):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    token.type = reserved.get(token.value.lower(),'ID')
+    return token
 
-    def t_NUM (self, token):
-        r'\d+'
-        return token
+def t_NUM (token):
+    r'\d+'
+    return token
 
-    def t_CF (self, token):
-        r'\n+'
-        t.lexer.lineno
+def t_CF (token):
+    r'\n+'
+    token.lexer.lineno
 
-    def t_STRING (self, token):
-        r'[\'|\"'
+def t_STRING (token):
+    r'[\']|[\"][a-zA-Z_0-9]*[\'|\"]'
+    return token
 
-    def t_COMMENT (self, token):
-        r'[\*[[.|\n]?]\*]|[\-\-.]'
-        token.type = self.reserved.get(t.value.lower(),'COMMENT')
-        return 
+def t_COMMENT (token):
+    r'\"[a-zA-Z_0-9_\W]*\"'
+    token.type = 'COMMENT'
+    return token
+
+def t_error (token):
+    print (f'Caractere ilegal {token.value[0]}')
+    token.lexer.skip(1)
+
+    
+
+tokens_list = []
+#import pdb; pdb.set_trace()
+for file_in_name in range(1, len(sys.argv)):
+    
+# leitura do arquivo para a memoria
+    file_name = str(sys.argv[file_in_name]).split('.txt')
+    working_file =  file_name[0] + '_Output' + '.wrk'
+    output_file = open(working_file, 'w')
+    with open(str(sys.argv[file_in_name]), 'r') as source:
+        source_text = source.read()
+        lexical = lex.lex()
+        lexical.input(source_text)
+        while True:
+            tkn = lexical.token()
+            if not tkn:
+                break
+            tokens_list.append(tkn)
+        for element in tokens_list:
+            print (element)
