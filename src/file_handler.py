71301381a3d198a69cd46e53d6eb187c74e78914
arumeng <mtsarumeida@gmail.com>
2022-09-26 14:35:41 -0300

    changes to file handler

--- src/file_handler.py
+++ src/file_handler.py
@@ -3,6 +3,6 @@ import sys, os
 def handler(file_name):
         working_file =  file_name[0] + '_Output.wrk'
         output_file = open(working_file, 'w')
-        with open(file_name[0]+'.txt', 'r') as source:
+        with open(file_name[0]+'.cl', 'r') as source:
             source_text = source.read()
         return source_text, output_file
